804ed26817ff0f0a2107677e4acf2febb88dbc86
giacomo <franco.mfg@alma.it>
2024-09-09 16:40:02 +0200

    errore in delete

--- sqlitedb.py
+++ sqlitedb.py
@@ -13,9 +13,11 @@ class SqliteDB:
     print('sqlite destructor')
     if self.conn:
       self.conn.close()
+      self.conn=None
     if self.cur:
       self.cur.close()
-    
+      self.cur=None
+
   def execute(self, query, params=None):
     self.cur = self.conn.cursor()
     if params is None:
@@ -26,10 +28,10 @@ class SqliteDB:
 
   def table_as_pd(self, table_name:str):
     return self.query_as_pd(f"SELECT * FROM {table_name}")
-    
+
   def query_as_pd(self, query, params=None):
     return pd.read_sql_query(query, self.conn, params=params)
-    
+
 if __name__ == "__main__":
    print('*** main ***')
    # from utils import download_url, unzip
